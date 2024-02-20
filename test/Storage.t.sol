c542e19ea6e76deadbd4eef17234fa3cb2a7de1d
Miguel_LZPF <mgcdreamer@gmail.com>
2024-02-20 18:38:59 +0100

    Add .prettierrc.json and update foundry.toml

--- test/Storage.t.sol
+++ test/Storage.t.sol
@@ -6,80 +6,80 @@ import {Storage, IStorage, IPayableOwner} from "@src/Storage.sol";
 import {DeployStorage} from "@script/DeployStorage.s.sol";
 
 contract StorageTest is Test {
-    // Constants
-    uint256 private constant INIT_AMOUNT = 12 ether;
-    address private constant DEFAULT_USER = address(10);
-    uint256 private constant DEFAULT_USER_BALANCE = 100 ether;
-    // Variables
-    Storage public myStorage;
-    address admin = DEFAULT_SENDER;
-    address payable user = payable(DEFAULT_USER);
+  // Constants
+  uint256 private constant INIT_AMOUNT = 12 ether;
+  address private constant DEFAULT_USER = address(10);
+  uint256 private constant DEFAULT_USER_BALANCE = 100 ether;
+  // Variables
+  Storage public myStorage;
+  address admin = DEFAULT_SENDER;
+  address payable user = payable(DEFAULT_USER);
 
-    function setUp() public {
-        // Transfer some ether to user
-        vm.deal(user, DEFAULT_USER_BALANCE);
-        // Deploy the contract
-        myStorage = new DeployStorage().run(INIT_AMOUNT);
-        // Check the initial state
-        assertEq(myStorage.retrieve(), INIT_AMOUNT);
-        assertEq(myStorage.hasRole(bytes32(0), address(admin)), true);
-        assertEq(myStorage.hasRole(bytes32(0), address(user)), false);
-    }
+  function setUp() public {
+    // Transfer some ether to user
+    vm.deal(user, DEFAULT_USER_BALANCE);
+    // Deploy the contract
+    myStorage = new DeployStorage().run(INIT_AMOUNT);
+    // Check the initial state
+    assertEq(myStorage.retrieve(), INIT_AMOUNT);
+    assertEq(myStorage.hasRole(bytes32(0), address(admin)), true);
+    assertEq(myStorage.hasRole(bytes32(0), address(user)), false);
+  }
 
-    function test_Store() public {
-        vm.startPrank(user);
-        uint256 newAmount = 3;
-        uint previousAmount = myStorage.retrieve();
-        assertEq(previousAmount, INIT_AMOUNT);
-        // Event check
-        vm.expectEmit(address(myStorage));
-        emit IStorage.Stored(newAmount);
-        // Change the value
-        myStorage.store(newAmount);
-        uint currentAmount = myStorage.retrieve();
-        assertEq(currentAmount, newAmount);
-        vm.stopPrank();
-    }
+  function test_Store() public {
+    vm.startPrank(user);
+    uint256 newAmount = 3;
+    uint previousAmount = myStorage.retrieve();
+    assertEq(previousAmount, INIT_AMOUNT);
+    // Event check
+    vm.expectEmit(address(myStorage));
+    emit IStorage.Stored(newAmount);
+    // Change the value
+    myStorage.store(newAmount);
+    uint currentAmount = myStorage.retrieve();
+    assertEq(currentAmount, newAmount);
+    vm.stopPrank();
+  }
 
-    function test_PayMe() public {
-        vm.startPrank(user);
-        uint256 amount = 15 ether;
-        // Initial state check
-        uint256 initBalanceUser = address(user).balance;
-        uint256 initBalanceAdmin = address(admin).balance;
-        assertGt(initBalanceUser, 1);
-        assertGt(initBalanceAdmin, 1);
-        // Event check
-        vm.expectEmit(address(myStorage));
-        emit IPayableOwner.ThankYou(address(admin), address(user), "Thanks!!");
-        // Pay the admin
-        myStorage.payMe{value: amount}();
-        // Final state check
-        uint256 finalBalanceUser = address(user).balance;
-        uint256 finalBalanceAdmin = address(admin).balance;
-        assertEq(finalBalanceUser, initBalanceUser - amount);
-        assertEq(finalBalanceAdmin, initBalanceAdmin + amount);
-        vm.stopPrank();
-    }
+  function test_PayMe() public {
+    vm.startPrank(user);
+    uint256 amount = 15 ether;
+    // Initial state check
+    uint256 initBalanceUser = address(user).balance;
+    uint256 initBalanceAdmin = address(admin).balance;
+    assertGt(initBalanceUser, 1);
+    assertGt(initBalanceAdmin, 1);
+    // Event check
+    vm.expectEmit(address(myStorage));
+    emit IPayableOwner.ThankYou(address(admin), address(user), "Thanks!!");
+    // Pay the admin
+    myStorage.payMe{value: amount}();
+    // Final state check
+    uint256 finalBalanceUser = address(user).balance;
+    uint256 finalBalanceAdmin = address(admin).balance;
+    assertEq(finalBalanceUser, initBalanceUser - amount);
+    assertEq(finalBalanceAdmin, initBalanceAdmin + amount);
+    vm.stopPrank();
+  }
 
-    function test_PayMe_ShouldNot_WhenBalance0() public {
-        address noBalanceAddress = payable(address(0));
-        vm.startPrank(noBalanceAddress);
-        uint256 amount = 1 ether;
-        // Initial state check
-        uint256 initBalanceUser = address(noBalanceAddress).balance;
-        uint256 initBalanceAdmin = address(admin).balance;
-        assertEq(initBalanceUser, 0);
-        assertGt(initBalanceAdmin, 1);
-        // Event check
-        vm.expectRevert();
-        // Pay the admin
-        myStorage.payMe{value: amount}();
-        // Final state check
-        uint256 finalBalanceUser = address(noBalanceAddress).balance;
-        uint256 finalBalanceAdmin = address(admin).balance;
-        assertEq(finalBalanceUser, initBalanceUser);
-        assertEq(finalBalanceAdmin, initBalanceAdmin);
-        vm.stopPrank();
-    }
+  function test_PayMe_ShouldNot_WhenBalance0() public {
+    address noBalanceAddress = payable(address(0));
+    vm.startPrank(noBalanceAddress);
+    uint256 amount = 1 ether;
+    // Initial state check
+    uint256 initBalanceUser = address(noBalanceAddress).balance;
+    uint256 initBalanceAdmin = address(admin).balance;
+    assertEq(initBalanceUser, 0);
+    assertGt(initBalanceAdmin, 1);
+    // Event check
+    vm.expectRevert();
+    // Pay the admin
+    myStorage.payMe{value: amount}();
+    // Final state check
+    uint256 finalBalanceUser = address(noBalanceAddress).balance;
+    uint256 finalBalanceAdmin = address(admin).balance;
+    assertEq(finalBalanceUser, initBalanceUser);
+    assertEq(finalBalanceAdmin, initBalanceAdmin);
+    vm.stopPrank();
+  }
 }
