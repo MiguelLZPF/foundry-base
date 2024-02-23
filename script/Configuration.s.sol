083702a3f48de32b3d3fb16968dfdaafd01b9a24
Miguel_LZPF <mgcdreamer@gmail.com>
2024-02-23 20:24:11 +0100

    Add getNetwork function to retrieve network information

--- script/Configuration.s.sol
+++ script/Configuration.s.sol
@@ -81,4 +81,68 @@ contract Configuration is Script {
     // Decode the deployment and return it
     return abi.decode(encDeployment, (Deployment));
   }
+
+  /**
+   * @dev Retrieves the network information.
+   * @return chainId The chain ID of the network.
+   * @return networkName The name of the network.
+   */
+  function getNetwork() external view returns (uint256 chainId, string memory networkName) {
+    chainId = block.chainid;
+    networkName = chainId == 31337 ? "anvil" : chainId == 1 ? "mainnet" : chainId == 3
+      ? "ropsten"
+      : chainId == 4
+      ? "rinkeby"
+      : chainId == 5
+      ? "goerli"
+      : chainId == 42
+      ? "kovan"
+      : chainId == 56
+      ? "binance"
+      : chainId == 97
+      ? "bsc-testnet"
+      : chainId == 128
+      ? "heco"
+      : chainId == 256
+      ? "heco-testnet"
+      : chainId == 137
+      ? "matic"
+      : chainId == 80001
+      ? "mumbai"
+      : chainId == 43114
+      ? "avalanche"
+      : chainId == 43113
+      ? "fuji"
+      : chainId == 1666700000
+      ? "harmony"
+      : chainId == 1666600000
+      ? "harmony-testnet"
+      : chainId == 42161
+      ? "arbitrum"
+      : chainId == 421611
+      ? "arbitrum-testnet"
+      : chainId == 250
+      ? "fantom"
+      : chainId == 4002
+      ? "celo"
+      : chainId == 44787
+      ? "moonbeam"
+      : chainId == 246
+      ? "zelcore"
+      : chainId == 1287
+      ? "moonriver"
+      : chainId == 43120
+      ? "avalanche-testnet"
+      : chainId == 43110
+      ? "avax"
+      : chainId == 4310
+      ? "fuji-testnet"
+      : chainId == 5777
+      ? "ganache"
+      : chainId == 31313
+      ? "hardhat"
+      : "unknown";
+
+    return (chainId, networkName);
+  }
 }
